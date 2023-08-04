86d2dfd4abaf698bece5d37ffd185928f87d806c
vanessah-26 <140348469+vanessah-26@users.noreply.github.com>
2023-08-03 18:58:53 -0700

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -3,6 +3,15 @@
 import argparse
 import glob
 import os
+# added here
+import mlflow    
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score
+from mlflow.models import infer_signature
+#from mlflow.utils.environment import _mlflow_conda_env
+from sklearn.model_selection import train_test_split
+
+
 
 import pandas as pd
 
@@ -12,6 +21,13 @@ from sklearn.linear_model import LogisticRegression
 # define functions
 def main(args):
     # TO DO: enable autologging
+    mlflow.autolog(log_models= True)   #enable the model to aumatically log, if False, will prevent from automatically log, as it is done manually later
+    model = XGBClassifier(use_label_encoder = False, eval_metrics = "logloss")
+    model.fit(X_train, y_train, eval_set = [(X_test, y_test)], verbose = False)  #when the fit() method is called on the pipeline object, the model will be autologged
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    
 
 
     # read data
@@ -34,7 +50,8 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
-
+def split_data(X_train, y_train, X_test, y_test):
+    X_train, y_train, X_test, y_test = train_test_split(dataframe, test_size = 0.4, random_state= 1) #not sure if they want the dataframe or what
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
